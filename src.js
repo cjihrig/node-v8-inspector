997a07c6d6812b70a1df5d5179b35355147be57d
cjihrig <cjihrig@gmail.com>
2016-06-07 12:49:25 -0400

    don't hardcode debug URL. Closes #1

--- src.js
+++ src.js
@@ -1,13 +1,39 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function onLoad() {
-  var launchButton = document.getElementById('launch');
-  var portField = document.getElementById('port');
-  var url = 'chrome-devtools://devtools/remote/serve_file/' +
-            '@521e5b7e2b7cc66b4006a8a54cb9c4e57494a5ef/inspector.html' +
-            '?experiments=true&v8only=true&ws=localhost:{PORT}/node';
+  const launchButton = document.getElementById('launch');
+  const portField = document.getElementById('port');
 
   launchButton.addEventListener('click', function onClick() {
-    var port = +portField.value || 5858;
+    const port = +portField.value || 5858;
+    const jsonUrl = `http://localhost:${port}/json/list`;
 
-    chrome.tabs.create({ url: url.replace('{PORT}', port) });
+    fetch(jsonUrl)
+      .then(function parseJson (response) {
+        if (response.status !== 200) {
+          throw new Error(`Invalid configuration data at ${jsonUrl}`);
+        }
+
+        return response.json();
+      })
+      .then(function getId (data) {
+        try {
+          return data[0].devtoolsFrontendUrl.match(/\/(@\w+)\//)[1];
+        } catch (err) {
+          throw new Error(
+            `Invalid devtools URL in ${JSON.stringify(data, null, 2)}`
+          );
+        }
+      })
+      .then(function openInspector (id) {
+        const url = `chrome-devtools://devtools/remote/serve_file/` +
+                    `${id}/inspector.html` +
+                    `?experiments=true&v8only=true&ws=localhost:${port}/node`;
+
+        chrome.tabs.create({ url });
+      })
+      .catch(function errorHandler (err) {
+        msg.innerHTML = `Could not launch debugger<br>${err.message}`;
+      });
   }, false);
 }, false);
