21a78802eec971f82d0fc7f9b3f99295f521ac03
Adam Bretz <arbretz@gmail.com>
2016-06-07 15:43:00 -0400

    Added some style. Misc JS cleanup. (#2)

--- src.js
+++ src.js
@@ -1,13 +1,11 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function onLoad() {
-  const launchButton = document.getElementById('launch');
-  const portField = document.getElementById('port');
 
-  launchButton.addEventListener('click', function onClick() {
-    const port = +portField.value || 5858;
-    const jsonUrl = `http://localhost:${port}/json/list`;
-
+  launch.addEventListener('click', function onClick() {
+    const portValue = +port.value || 5858;
+    const jsonUrl = `http://localhost:${portValue}/json/list`;
+    error.style.display = 'none';
     fetch(jsonUrl)
       .then(function parseJson (response) {
         if (response.status !== 200) {
@@ -28,12 +26,13 @@ document.addEventListener('DOMContentLoaded', function onLoad() {
       .then(function openInspector (id) {
         const url = `chrome-devtools://devtools/remote/serve_file/` +
                     `${id}/inspector.html` +
-                    `?experiments=true&v8only=true&ws=localhost:${port}/node`;
+                    `?experiments=true&v8only=true&ws=localhost:${portValue}/node`;
 
         chrome.tabs.create({ url });
       })
       .catch(function errorHandler (err) {
-        msg.innerHTML = `Could not launch debugger<br>${err.message}`;
+        error.innerHTML = `Could not launch debugger<br>${err.message}`;
+        error.style.display = 'block';
       });
   }, false);
 }, false);
